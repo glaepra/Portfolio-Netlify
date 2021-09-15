2809c518145dcce7e890fafe0323d31a2a93c6ed
Victor Dorand <dorandvictor@gmail.com>
2021-09-15 16:59:56 +0200

    progressive webapp

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 	Pour mieux comprendre ce script, voir : https://css-tricks.com/serviceworker-for-offline/
 *******************/
 
-var version = 'v1:0:0';
+var version = 'v1:0:1';
 
 self.addEventListener("install", function(event) {
 	self.skipWaiting();
