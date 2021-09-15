6bfe414058b3c47705c2de3bf73ae7f6ec9ae4ab
Victor Dorand <dorandvictor@gmail.com>
2021-09-15 16:57:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,8 +11,8 @@ self.addEventListener("install", function(event) {
       .then(function(cache) {
         return cache.addAll([
           '/',
-          'index.html',
-          'manifest.json'
+          'https://portfolio-victor-dorand.netlify.app/index.html',
+          'https://portfolio-victor-dorand.netlify.app/manifest.json'
         ]);
       })
 	);
