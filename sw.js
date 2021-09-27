267632097191938cbe33baa8a3088c9a51e77c84
Victor Dorand <dorandvictor@gmail.com>
2021-09-27 09:42:57 +0200

    ok

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 	Pour mieux comprendre ce script, voir : https://css-tricks.com/serviceworker-for-offline/
 *******************/
 
-var version = 'v1:0:8';
+var version = 'v1:0:9';
 
 self.addEventListener("install", function(event) {
 	self.skipWaiting();
