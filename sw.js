bf4c0543278b8e8cf4850a3d3a6759bfbd80728a
Victor Dorand <dorandvictor@gmail.com>
2022-08-02 17:01:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 	Pour mieux comprendre ce script, voir : https://css-tricks.com/serviceworker-for-offline/
 *******************/
 
-var version = 'v1:1:5';
+var version = 'v1:1:6';
 
 self.addEventListener("install", function(event) {
     self.skipWaiting();
